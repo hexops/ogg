114d7c55b5b716c230665705a133e2e196069720
Ali Chraghi <alichraghi@proton.me>
2023-07-29 23:52:44 +0330

    build.zig

---
+++ build.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "ogg",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibC();
+    lib.addIncludePath("include");
+    lib.addCSourceFiles(&sources, &.{"-fno-sanitize=undefined"});
+    lib.installHeadersDirectory("include/ogg", "ogg");
+    b.installArtifact(lib);
+}
+
+const sources = [_][]const u8{
+    "src/bitwise.c",
+    "src/framing.c",
+};
