be7be18acc6bec07b8a95d3792022184e92581dd
Ali Chraghi <alichraghi@proton.me>
2023-08-05 06:00:21 +0330

    build: add config_header values

--- build.zig
+++ build.zig
@@ -4,10 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const config_header = b.addConfigHeader(.{
-        .style = .{ .autoconf = .{ .path = "include/ogg/config_types.h.in" } },
-        .include_path = "config_types.h",
-    }, .{});
+    const config_header = b.addConfigHeader(
+        .{
+            .style = .{ .cmake = .{ .path = "include/ogg/config_types.h.in" } },
+            .include_path = "ogg/config_types.h",
+        },
+        .{
+            .INCLUDE_INTTYPES_H = 0,
+            .INCLUDE_STDINT_H = 1,
+            .INCLUDE_SYS_TYPES_H = 0,
+            .SIZE16 = .int16_t,
+            .USIZE16 = .uint16_t,
+            .SIZE32 = .int32_t,
+            .USIZE32 = .uint32_t,
+            .SIZE64 = .int64_t,
+            .USIZE64 = .uint64_t,
+        },
+    );
 
     const lib = b.addStaticLibrary(.{
         .name = "ogg",
