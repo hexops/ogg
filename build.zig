a55253b4cd811288d378f5d99df9a92cf6eac0f7
Stephen Gutekanst <stephen@hexops.com>
2023-10-17 20:08:44 -0700

    build: Use ConfigHeader to generate config_types.h
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -4,15 +4,42 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const config_header = b.addConfigHeader(
+        .{
+            .style = .{ .cmake = .{ .path = "include/ogg/config_types.h.in" } },
+            .include_path = "ogg/config_types.h",
+        },
+        .{
+            .INCLUDE_INTTYPES_H = 0,
+            .INCLUDE_STDINT_H = 1,
+            .INCLUDE_SYS_TYPES_H = 0,
+            .SIZE16 = .int16_t,
+            .USIZE16 = .uint16_t,
+            .SIZE32 = .int32_t,
+            .USIZE32 = .uint32_t,
+            .SIZE64 = .int64_t,
+            .USIZE64 = .uint64_t,
+        },
+    );
+
     const lib = b.addStaticLibrary(.{
         .name = "ogg",
         .target = target,
         .optimize = optimize,
     });
     lib.linkLibC();
+    lib.addConfigHeader(config_header);
     lib.addIncludePath(.{ .path = "include" });
     lib.addCSourceFiles(.{ .files = &sources, .flags = &.{"-fno-sanitize=undefined"} });
-    lib.installHeadersDirectory("include/ogg", "ogg");
+    lib.installHeadersDirectoryOptions(.{
+        .source_dir = .{ .path = "include/ogg" },
+        .install_dir = .header,
+        .install_subdir = "ogg",
+        .exclude_extensions = &.{".in"},
+    });
+    lib.installConfigHeader(config_header, .{
+        .dest_rel_path = "ogg/config_types.h",
+    });
     b.installArtifact(lib);
 }
 
